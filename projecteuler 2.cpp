ac608b8e01eed31b8c013d0a72173fa9a8370592
cinaratac <163647886+cinaratac@users.noreply.github.com>
2024-09-28 13:31:43 +0300

    Add files via upload

---
+++ projecteuler 2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int a = 1;
+	int b = 1;
+	int nextfib;
+	int sum = 0;
+	while (true) {
+		nextfib = a + b;
+		if (nextfib >= 4000000) break;
+	
+		if (nextfib % 2 == 0) {
+			sum += nextfib;
+		}
+		a = b;
+		b = nextfib;
+	
+	
+	}
+	cout << "toplam:" << sum << endl;
+
+
+
+
+
+}
